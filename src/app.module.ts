de00aee3fe4c161e58cbf079545df2484d98072e
John Angel <usw4763@gmail.com>
2021-10-18 20:16:52 -0500

    Adding user/auth modules/services/controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FeedbackModule } from './feedback/modules/feedback.module';
 import { FeedbackEntity } from './feedback/entities/feedback.entity';
+import { UserController } from './user/controllers/user.controller';
+import { UserModule } from './user/modules/user.module';
+import { AuthModule } from './auth/modules/auth.module';
 
 @Module({
   imports: [
@@ -17,8 +20,10 @@ import { FeedbackEntity } from './feedback/entities/feedback.entity';
       entities: [FeedbackEntity],
     }),
     FeedbackModule,
+    UserModule,
+    AuthModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
